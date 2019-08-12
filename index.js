75981782db1d5a1377270173b327cd4542f0fb3a
Chris Aquino <chris@digitalcrafts.com>
2019-08-12 11:05:15 -0400

    shows steps of substitution

--- index.js
+++ index.js
@@ -9,3 +9,13 @@
 // 1b. Grab specific sound *before* the assignment
 // const catSound = require("./cat").cat;
 console.log(require("./cat").cat);
+console.log(require("./cat").cat); // Start with "./cat" - do we sub?
+console.log({
+    cat: catSaysWhat,      // In JS, quotes around keynames are optional
+    kitten: kittenSaysWhat // unless you use dashes or spaces
+  }.cat); // Do we sub for require("./cat")
+console.log("purrrrrrr"); // Eventually arrives at simplest version.
+
+
+
+// 2. "Destructuring"
