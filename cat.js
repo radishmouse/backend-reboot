566ba731f138c3106f036a526c9245653ff55399
Chris Aquino <chris@digitalcrafts.com>
2019-08-12 10:47:51 -0400

    adds key access for exported object

--- cat.js
+++ cat.js
@@ -1,4 +1,10 @@
 const catSaysWhat = "purrrrrrr";
+const kittenSaysWhat = "squeak";
+
+const bagOfCats = {
+  cat: catSaysWhat,      // In JS, quotes around keynames are optional
+  kitten: kittenSaysWhat // unless you use dashes or spaces
+};
 
 // module.exports is built into Node.js
-module.exports = catSaysWhat;
+module.exports = bagOfCats;
--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-const catSound = require("./cat");
-console.log(catSound);
+// 0. Grab specific sound using .<keyname>
+// const catSound = require("./cat");
+// console.log(catSound.cat);
+
+// 1a. Grab specific sound *before* the assignment
+// const catSound = require("./cat").cat;
+// console.log(catSound);
+
+// 1b. Grab specific sound *before* the assignment
+// const catSound = require("./cat").cat;
+console.log(require("./cat").cat);
