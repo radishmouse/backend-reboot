427d924916e357f5f284b7514dc81e95f663cb9c
Chris Aquino <chris@digitalcrafts.com>
2019-08-12 11:12:54 -0400

    adds destructuring example

--- index.js
+++ index.js
@@ -8,14 +8,20 @@
 
 // 1b. Grab specific sound *before* the assignment
 // const catSound = require("./cat").cat;
-console.log(require("./cat").cat);
-console.log(require("./cat").cat); // Start with "./cat" - do we sub?
-console.log({
-    cat: catSaysWhat,      // In JS, quotes around keynames are optional
-    kitten: kittenSaysWhat // unless you use dashes or spaces
-  }.cat); // Do we sub for require("./cat")
-console.log("purrrrrrr"); // Eventually arrives at simplest version.
+// console.log(require("./cat").cat);
+// console.log(require("./cat").cat); // Start with "./cat" - do we sub?
+// console.log({
+//     cat: catSaysWhat,      // In JS, quotes around keynames are optional
+//     kitten: kittenSaysWhat // unless you use dashes or spaces
+//   }.cat); // Do we sub for require("./cat")
+// console.log("purrrrrrr"); // Eventually arrives at simplest version.
 
 
 
-// 2. "Destructuring"
+// 2. "Destructuring" - "pluck a specific value out of an object"
+const {cat, kitten} = require("./cat");
+console.log(cat);
+console.log(kitten);
+// When would I destructure?
+// When you want one to three individual values.
+// If you want a lot more, just bring in the whole object.
